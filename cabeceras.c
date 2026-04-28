6590fb4adba569021307040c9afa6356ffcebe32
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 12:07:16 -0500

    Primer commit

---
+++ cabeceras.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "sumar.h"
+
+int main(int argc, char const * argv[])
+{
+    int a, b;
+    printf("Ingrese 1er numero");
+    scanf("%d", &a);
+    printf("Ingrese 2do numero");
+    scanf("%d", &b);
+    printf("La suma es: %d", sumar(a,b));
+    return 0;
+}
